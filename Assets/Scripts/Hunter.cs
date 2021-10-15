662fb8e6545baf3338ea66f5d8d73818590a3c72
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-10-16 01:48:01 +0900

    Playerの動きを追加、Test用ファイルの作成

---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : CharaBase
+{ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        base.Move();
+    }
+}
