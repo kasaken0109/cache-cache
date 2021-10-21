1d19c0c8446949db2edd1d87faea6443d9b0edde
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-10-21 16:29:25 +0900

    攻撃した時の処理を行うクラスを作成した

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Animal")) GetComponentInParent<Hunter>().PlayStun();
+        else if (collision.CompareTag("Witch")) collision.GetComponent<Witch>().OnHit();
+    }
+}
