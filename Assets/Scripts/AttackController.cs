b94e9c35e1154efde3a0e27df315c5e15e6b94ad
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-10-23 20:20:12 +0900

    攻撃処理修正

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -15,20 +15,20 @@ public class AttackController : MonoBehaviour
         }
     }
     bool IsFirst = true;
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Animal") && IsFirst)
-        {
-            GetComponentInParent<Hunter>().PlayStun();
-            IsFirst = false;
-        }
-        else if (collision.CompareTag("Witch") && IsFirst)
-        {
-            collision.GetComponent<Witch>().OnHit();
-            Debug.Log("HunterAttack");
-            IsFirst = false;
-        }
-    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Animal") && IsFirst)
+    //    {
+    //        GetComponentInParent<Hunter>().PlayStun();
+    //        IsFirst = false;
+    //    }
+    //    else if (collision.CompareTag("Witch") && IsFirst)
+    //    {
+    //        collision.GetComponent<Witch>().OnHit();
+    //        Debug.Log("HunterAttack");
+    //        IsFirst = false;
+    //    }
+    //}
 
     private void OnEnable()
     {
