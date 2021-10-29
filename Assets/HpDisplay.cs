7c14b695a985f2952133158a9669d5e2991c2f34
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-10-29 15:52:32 +0900

    HPを表示するクラスを追加した

---
+++ Assets/HpDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class HpDisplay : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("HPを表示する画像")]
+    private Image[] images = default;
+
+    [SerializeField]
+    [Tooltip("減るエフェクトのスピード")]
+    private float m_speed = 1f;
+
+    [SerializeField]
+    [Tooltip("HPを表示するオブジェクト")]
+    private GameObject m_display = null;
+
+
+    PhotonView m_view = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_view = GetComponent<PhotonView>();
+        m_display.SetActive(m_view.IsMine);
+    }
+
+    // Update is called once per frame
+    public void UpdateHp(int hp)
+    {
+        StartCoroutine(UIEffect(images[hp]));
+    }
+
+    IEnumerator UIEffect(Image target)
+    {
+        while (target.fillAmount >= 0.01f)
+        {
+            target.fillAmount -= 0.01f * m_speed;
+            yield return null;
+        }
+    }
+}
