1f9e88cd45c455b8bc0df2e1c0a3738880cf2618
forest374 <74588967+forest374@users.noreply.github.com>
2021-10-29 13:04:34 +0900

    観戦カメラ

---
+++ Assets/Scripts/WitchCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WitchCamera : MonoBehaviour
+{
+    [Header("カメラの速度"), SerializeField]
+    float m_speed = 3f;
+
+    PhotonView m_view = default;
+
+
+    void Start()
+    {
+        m_view = GetComponent<PhotonView>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!m_view || !m_view.IsMine) return;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (h != 0|| v != 0)
+        {
+            transform.position += new Vector3(h / 10, v / 10);
+        }
+    }
+}
