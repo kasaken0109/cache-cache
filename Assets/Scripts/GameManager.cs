a822c78afff62d20f4bd2c4b0532629f1dcece05
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-10-29 19:59:12 +0900

    シーン遷移,ロビー作成手抜き

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ using Photon.Realtime;
 
 public enum NetworkEvents : byte
 {
+    //ゲームスタート前のロビーにいる状態
+    Lobby,
     GameStart,
     //魔女側の勝ち
     Win,
@@ -16,38 +18,54 @@ public enum NetworkEvents : byte
     Die,
 }
 public class GameManager : MonoBehaviourPunCallbacks, IOnEventCallback
-{
-    [SerializeField]
+{  
     CharactorSpawn m_charactorSpawn;
+    AnimalManager m_animalManager;
+    /// <summary>このクラスのインスタンスが既にあるかどうか</summary>
+    static bool m_isExists = false;
     int m_witchDieCount;
+    int m_witchCurrentCount;
+    void Awake()
+    {
+        if (m_isExists)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_isExists = true;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        m_animalManager = GameObject.Find("AnimalManager").GetComponent<AnimalManager>();
+    }
 
     public void OnEvent(EventData photonEvent)
     {
         switch (photonEvent.Code)
         {
+            case (byte)NetworkEvents.Lobby:
+
+                break;
             case (byte)NetworkEvents.GameStart:
+                m_animalManager.StartSpawn();
                 break;
             case (byte)NetworkEvents.Win:
                 Debug.Log("魔女の勝利");
+
                 break;
             case (byte)NetworkEvents.Lose:
                 Debug.Log("魔女の負け");
+
                 break;
             case (byte)NetworkEvents.Die:
                 Debug.Log("魔女が死んだ");
+                var judge = GameObject.Find("JudgementController").GetComponent<JudgementController>();
                 m_witchDieCount++;
-                LoseJudge();
+                judge.LoseJudge(m_witchDieCount);
                 break;
-        }
-    }
-    void LoseJudge()
-    {
-        if (m_witchDieCount >= m_charactorSpawn.WitchPositions.Length)
-        {
-            RaiseEventOptions raiseEventoptions = new RaiseEventOptions();
-            raiseEventoptions.Receivers = ReceiverGroup.All;
-            SendOptions sendOptions = new SendOptions();
-            PhotonNetwork.RaiseEvent((byte)NetworkEvents.Lose, null, raiseEventoptions, sendOptions);
         }
     }
 }
