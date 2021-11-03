6e5c5a3e0271b2ea2199705f6d2e264d998bacc9
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-04 04:50:12 +0900

    トラップを置く処理を作成した

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Trap : Item
+{
+    string m_trap = "Trap";
+    Hunter m_hunter = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_hunter = GameObject.FindGameObjectWithTag("Hunter").GetComponent<Hunter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) UseItem();
+    }
+
+    public override void UseItem()
+    {
+        PhotonNetwork.Instantiate(m_trap,m_hunter.transform.position,m_hunter.transform.rotation);
+        base.UseItem();
+    }
+}
