e648c3b68f3c064da5f042ed4189b70828208995
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-04 04:48:48 +0900

    アイテムの基底クラスを作成した

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("別のアイテムが使えるようになる時間")]
+    private float m_cooldown = 5f;
+
+    /// <summary>
+    /// 別のアイテムが使えるようになる時間
+    /// </summary>
+    public float CoolDown => m_cooldown;
+    // Start is called before the first frame update
+
+    public virtual void UseItem()
+    {
+        //アイテムごとの処理の関数
+        Destroy(this);
+    }
+}
