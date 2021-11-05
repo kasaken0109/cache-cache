f4a43cdda8e50372b24794de3f83f037fd58260e
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-05 15:36:49 +0900

    生成の処理を追加した

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -5,6 +5,9 @@ using Photon.Pun;
 
 public class Trap : Item
 {
+    [SerializeField]
+    [Tooltip("設置時間")]
+    float m_trapTime = 1f;
     string m_trap = "Trap";
     Hunter m_hunter = default;
     // Start is called before the first frame update
@@ -16,12 +19,39 @@ public class Trap : Item
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) UseItem();
+        //if (Input.GetButtonDown("Jump")) UseItem();
     }
-
+    GameObject[] traps;
     public override void UseItem()
     {
-        PhotonNetwork.Instantiate(m_trap,m_hunter.transform.position,m_hunter.transform.rotation);
+        traps = GameObject.FindGameObjectsWithTag("Trap");
+        StartCoroutine(nameof(WaitCharge));
+        GameObject trap = PhotonNetwork.Instantiate(m_trap, m_hunter.transform.position, m_hunter.transform.rotation);
+        if (traps.Length > 2)
+        {
+            PhotonNetwork.Destroy(GameObject.Find("trap1"));
+            traps = GameObject.FindGameObjectsWithTag("Trap");
+            for (int i = 0; i < traps.Length - 1; i++)
+            {
+                traps[i] = GameObject.Find("trap" + (i + 1)) ? GameObject.Find("trap" + (i + 1)) : GameObject.Find("trap" + (i + 2));
+                traps[i].name = "trap" + (i + 1).ToString();
+            }
+            trap.name = "trap" + traps.Length.ToString();
+        }
+        else
+        {
+            trap.name = "trap" + traps == null ? "trap" + "1" : "trap" + (traps.Length + 1).ToString();
+        }
         base.UseItem();
+    }
+
+    IEnumerator WaitCharge()
+    {
+        float timer = 0;
+        while (timer < m_trapTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
