e6590ea7d466d77fd4bbbbf59b961fde6a2aa19a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-05 15:33:13 +0900

    Trapに魔女が当たったときの処理を追加した

--- Assets/TrapController.cs
+++ Assets/TrapController.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class TrapController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Witch") && IsFirst)
+        {
+            PhotonView view = collision.gameObject.GetComponent<PhotonView>();
+
+            if (view)
+            {
+                view.RPC("PlayStun", RpcTarget.All);
+            }
+            IsFirst = false;
+        }
+    }
+    bool IsFirst = true;
+
+    private void OnEnable()
+    {
+        IsFirst = true;
     }
 }
