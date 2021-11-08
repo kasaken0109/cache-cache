2730542ebab5f400792bc93480f7daf00c8d08dd
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-08 10:59:51 +0900

    視野角を広げるアイテムクラスを作成した

---
+++ Assets/Scripts/EnhancedVisibility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnhancedVisibility : Item
+{
+    [SerializeField]
+    [Tooltip("効果時間")]
+    private int m_time = 20;
+
+    private HunterCamera m_hunterCamera = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_hunterCamera = GetComponentInChildren<HunterCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void UseItem()
+    {
+        StartCoroutine(nameof(Use));
+    }
+
+    IEnumerator Use()
+    {
+        StartCoroutine(m_hunterCamera.FoVChange(m_time));
+        yield return new WaitForSeconds(m_time + 2);
+        base.UseItem();
+    }
+}
