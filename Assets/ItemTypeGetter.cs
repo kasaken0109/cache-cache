dfa6bc383f428a0bf9a305ba0804f6c44e2cf3bd
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-08 12:21:37 +0900

    生成時IDの変数を追加した

--- Assets/ItemTypeGetter.cs
+++ Assets/ItemTypeGetter.cs
@@ -8,5 +8,9 @@ public class ItemTypeGetter : MonoBehaviour
     Hunter.HaveItemType itemType = Hunter.HaveItemType.None;
 
     public Hunter.HaveItemType ItemType => itemType;
+
+    private int m_id = 0;
+
+    public int ID { get => m_id; set { m_id = value; } }
     
 }
