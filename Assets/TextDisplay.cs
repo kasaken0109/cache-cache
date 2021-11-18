fb65a0d92eed34e8fc1ea66c766a378231749cf1
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-18 21:50:43 +0900

    GameManager内のオブジェクト参照の処理を変更した

---
+++ Assets/TextDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("勝者名の表示箇所")]
+    Text m_winner = default;
+
+    [SerializeField]
+    [Tooltip("敗者名の表示箇所")]
+    Text m_loser = default;
+
+    public void ShowResult()
+    {
+        m_winner.text = ShowTextCtrl.GetLogData();
+        m_loser.text = ShowTextCtrl.GetLogData();
+    }
+}
