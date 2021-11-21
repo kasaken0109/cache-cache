3d879e2fd958a1c2843ab1e62fd94768b07bdfa8
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-22 02:24:21 +0900

    階層情報を表示するクラスを作成した

---
+++ Assets/LayerDisplay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LayerDisplay : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("階層を表示するUI")]
+    Text m_display = default;
+
+    [SerializeField]
+    [Tooltip("デバッグモード")]
+    bool m_debug = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.CompareTag("Hunter") || collision.CompareTag("Witch")) 
+            && (m_debug || collision.GetComponent<PhotonView>().IsMine) && collision.transform.position.z == transform.position.z)
+        {
+            Debug.Log("Enter");
+            m_display.text = gameObject.name;
+        }
+    }
+
+    bool IsFirst = true;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((collision.CompareTag("Hunter") || collision.CompareTag("Witch")) 
+            && (m_debug || collision.GetComponent<PhotonView>().IsMine) && collision.transform.position.z == transform.position.z && IsFirst)
+        {
+            m_display.text = gameObject.name;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((collision.CompareTag("Hunter") || collision.CompareTag("Witch")) 
+            && (m_debug || collision.GetComponent<PhotonView>().IsMine))
+        {
+            IsFirst = false;
+        }
+    }
+}
