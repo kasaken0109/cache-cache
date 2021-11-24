08b838ba862a040ee27803eec7df323dba0a82f4
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-11-24 16:56:01 +0900

    チェックのバグとシーンローダーの修正をしました。

--- Assets/Scripts/JudgementController.cs
+++ Assets/Scripts/JudgementController.cs
@@ -8,13 +8,11 @@ using UnityEngine.UI;
 
 public class JudgementController : MonoBehaviour
 {
-    [SerializeField] Image m_checkImage;
-    public int m_charaCount;
+    int m_charaCount;
     public void LoseJudge(int witchDieCount, int witchCapacity)
     {
         if (witchDieCount >= witchCapacity)
         {
-            Debug.Log(witchDieCount);
             RaiseEventOptions raiseEventoptions = new RaiseEventOptions();
             raiseEventoptions.Receivers = ReceiverGroup.All;
             SendOptions sendOptions = new SendOptions();
@@ -25,7 +23,6 @@ public class JudgementController : MonoBehaviour
     public void Check()
     {
         m_charaCount++;
-        m_checkImage.gameObject.SetActive(true);
         if (PhotonNetwork.IsMasterClient)
         {
             if (PhotonNetwork.CurrentRoom.MaxPlayers == m_charaCount)
@@ -41,6 +38,5 @@ public class JudgementController : MonoBehaviour
     public void UncChecked()
     {
         m_charaCount--;
-        m_checkImage.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,15 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    //private IEnumerator LoadScene(string sceneName)
-    //{
-    //    var scene = SceneManager.LoadSceneAsync(sceneName);
-    //    scene.allowSceneActivation = false;
-    //    yield return new WaitForSeconds(2);
-    //    scene.allowSceneActivation = true;
-    //}
-    public void LoadScene(string sceneName)
+    [SerializeField]
+    string m_sceneName;
+    [SerializeField]
+    int m_sceneIndex;
+
+    public IEnumerator LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+        yield return new WaitForSeconds(1);
+        scene.allowSceneActivation = true;
+    }
+    public IEnumerator LoadScene(int sceneIndex)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneIndex);
+        scene.allowSceneActivation = false;
+        yield return new WaitForSeconds(1);
+        scene.allowSceneActivation = true;
+    }
+    public IEnumerator LoadSceneName()
+    {
+        var scene = SceneManager.LoadSceneAsync(m_sceneName);
+        scene.allowSceneActivation = false;
+        yield return new WaitForSeconds(1);
+        scene.allowSceneActivation = true;
+    }
+    public IEnumerator LoadSceneIndex()
+    {
+        var scene = SceneManager.LoadSceneAsync(m_sceneIndex);
+        scene.allowSceneActivation = false;
+        yield return new WaitForSeconds(1);
+        scene.allowSceneActivation = true;
     }
 }
