ee6e6b327fdbf612e211fb68b9bdb853d1cc28d2
forest374 <74588967+forest374@users.noreply.github.com>
2021-11-26 15:37:49 +0900

    AnimalManagar変更

--- Assets/Scripts/AnimalManager.cs
+++ Assets/Scripts/AnimalManager.cs
@@ -10,36 +10,30 @@ public class AnimalManager : MonoBehaviour
         くま, うし, きつね, にわとり, animal
     }
 
-    [SerializeField, Header("出現数")]
-    int m_spawnNum = default;
+    [System.Serializable]
+    struct SpownAnimal
+    {
+        [SerializeField]
+        public AnimalList animal;
+        [SerializeField]
+        public int mapNum;
+        [SerializeField]
+        public int right;
+        [SerializeField]
+        public int left;
+        [SerializeField]
+        public int spownNum;
+    }
+    List<Animal> animals = new List<Animal>();
+
+    [SerializeField, Header("出現するz座標")]
+    int[] m_mapPos = new int[] { 0, -20, -40};
+
     [SerializeField, Header("出現させる動物")]
-    AnimalList m_animalList = AnimalList.animal;
-
-    string m_animalName = default;
-
-    Animal[] animals = default;
-    Vector2Int m_scale = default;
-    Vector2Int m_bottomLeft = default;
-    Vector2Int m_topRight = default;
-    List<Vector2> m_animalPos = new List<Vector2>();
-
+    SpownAnimal[] m_spownAnimals = new SpownAnimal[] { };
 
     public void StartSpawn()
     {
-        m_animalName = m_animalList.ToString();
-        m_scale = new Vector2Int((int)this.transform.localScale.x, (int)this.transform.localScale.y);
-        Vector2 pos = this.transform.position;
-        m_bottomLeft = new Vector2Int((int)pos.x - m_scale.x / 2, (int)pos.y - m_scale.y / 2);
-        m_topRight = new Vector2Int((int)pos.x + m_scale.x / 2, (int)pos.y + m_scale.y / 2);
-
-        if (m_spawnNum >= m_scale.x * m_scale.y)
-        {
-            Debug.Log("設定された数が多すぎます");
-            m_spawnNum = ((int)m_scale.x - 2) * ((int)m_scale.y - 2) - 1;
-            Debug.Log(m_spawnNum + "を設定しました");
-        }
-
-        animals = new Animal[m_spawnNum];
         Spawn();
         StartCoroutine(ManagerUpdate());
     }
@@ -62,27 +56,27 @@ public class AnimalManager : MonoBehaviour
     /// </summary>
     private void Spawn()
     {
-        for (int i = 0; i < m_spawnNum;)
+        bool samePos = false;
+        int pos = 0;
+        for (int i = 0; i < m_spownAnimals.Length; samePos = false)
         {
-            bool dup = false;
-            Vector2 pos = new Vector2(Random.Range(m_bottomLeft.x + 1, m_topRight.x), Random.Range(m_bottomLeft.y + 1, m_topRight.y));
-            // 同じ場所にスポーンしないようにする
-            for (int k = 0; k < m_animalPos.Count; k++)
+            if (m_spownAnimals[i].spownNum > m_spownAnimals[i].right - m_spownAnimals[i].left) 
             {
-                if (m_animalPos[k] == pos)
-                {
-                    dup = true;
-                    break;
-                }
+                Debug.LogError("設定している数を生成できません");
+                break;
             }
-            if (dup == true)
+
+            pos = Random.Range(m_spownAnimals[i].left, m_spownAnimals[i].right);
+            // 同じ位置にならないようにする
+            for (int k = 0; k < animals.Count; k++)
             {
-                Debug.Log("not");
-                continue;
+                if (animals[k].gameObject.transform.position.z == m_mapPos[m_spownAnimals[i].mapNum] &&
+                    pos == animals[k].gameObject.transform.position.x) samePos = true;
             }
-            animals[i] = PhotonNetwork.Instantiate(m_animalName, pos, Quaternion.identity).GetComponent<Animal>();
-            animals[i].CreateStart();
-            m_animalPos.Add(pos);
+            if (samePos) continue;
+
+            animals.Add(PhotonNetwork.Instantiate(m_spownAnimals[i].animal.ToString(), new Vector3(pos, 0, m_mapPos[m_spownAnimals[i].mapNum]), Quaternion.identity).GetComponent<Animal>());
+
             i++;
         }
     } 
