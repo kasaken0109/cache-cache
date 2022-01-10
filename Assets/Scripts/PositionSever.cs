0430106dabbf908cacc78d41da8658c28ae9bd91
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-11 00:32:02 +0900

    獣道の中で変身解除してしまうと上空に瞬間移動してしまうバグの解消

---
+++ Assets/Scripts/PositionSever.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PositionSever : MonoBehaviour
+{
+    LayerMask _layer;
+    Vector3 _savePos = Vector3.zero;
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.AddComponent<SavePosition>().SetUp(PostionUpDate);
+        }
+    }
+
+    void PostionUpDate(Vector3 savePos, GameObject obj)
+    {
+        _savePos = savePos;
+        _layer = obj.layer;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        PhotonView view = collision.gameObject.GetComponent<PhotonView>();
+        GameObject obj = collision.gameObject;
+        float posY = obj.transform.position.y;
+        if (view != null && view.IsMine && obj.CompareTag("Witch") && posY > 3)
+        {
+            obj.transform.position = _savePos;
+        }
+    }
+}
