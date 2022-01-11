9c3da70a3faddf36239f2f99e13f4592953d6896
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-01-12 05:58:58 +0900

    MiniMapの状態を変更するクラスを作成した

---
+++ Assets/MinimapIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIndicator : MonoBehaviour
+{
+    SpriteRenderer m_sr;
+    private void Start()
+    {
+        m_sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetEnable(bool value)
+    {
+        m_sr.enabled = value;
+    }
+}
