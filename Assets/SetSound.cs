dc09bed5fc0affdf84f792a654dbb0e934700d9f
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-13 15:19:35 +0900

    SetSoundの追加

---
+++ Assets/SetSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSound : MonoBehaviour
+{
+    enum BGMType
+    {
+        Main = 0,
+        Game = 1,
+    }
+
+    [SerializeField] BGMType _type;
+    void Start()
+    {
+        Sounds.SoundMaster.Request(null, (int)_type, 0);
+    }
+
+    public void OnClick(int id)
+    {
+        Sounds.SoundMaster.Request(null, id, 1);
+    }
+}
